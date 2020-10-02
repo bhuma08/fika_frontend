df3b1bf6fa685b29432c64ee6721784373ed0780
bhuma08 <62709043+bhuma08@users.noreply.github.com>
2020-10-02 10:33:51 +0100

    cleaning up

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,50 +1,10 @@
 import React, { useState } from "react";
 import Board from "./Board";
 import GameChat from "./GameChat";
-// import { connect } from "react-redux";
-// import { getUser } from "../Actions/actions";
 import Modal from 'react-modal';
-// import { Modal } from 'react-responsive-modal';
+
 import "../styles/tictactoe.css";
 
-// class Game extends Component {
-
-//   state = {
-
-//     open: false
-//   }
-
-//   onOpenModal = () => {
-//     this.setState({ open: true });
-//   };
-
-//   onCloseModal = () => {
-//     this.setState({ open: false });
-//   };
-
-//   render() {
-
-//     const {open} = this.state
-//     return (
-//       <>
-//         <div >
-//             <Board /> 
-
-//             <button onClick={this.onOpenModal}>Game Chat</button>
-
-//             <Modal open={open} onClose={this.onCloseModal} center>
-//                 <GameChat/>
-//             </Modal> 
-
-//         </div>
-
-//       </>
-//     );
-//   }
-// }
-// const mSTP = (state) => ({ user: state });
-
-// export default connect(mSTP, { getUser })(Game);
 const customStyles = {
   content : {
     top                   : '50%',
@@ -54,7 +14,7 @@ const customStyles = {
     // marginRight           : '-50%',
     marginLeft            :'17%',
     
-    transform             : 'translate(-210%, -30%)',
+    transform             : 'translate(-5%, -30%)',
     backgroundColor       : 'white'
   }
 };
@@ -72,13 +32,18 @@ function Game (){
   }
 
   return(
-    <div>
+
+    <div className='container'>
+
       <div><Board /> </div>
-      <button className='gameChatBTN'onClick={setModalTrue}>Game Chat</button>
-      <Modal isOpen={modalIsOpen} shouldCloseOnOverlayClick={true} onRequestClose={()=> setModalIsOpen(false)} style={customStyles}>
-          <button onClick={setModalFalse}>x</button>
-          <GameChat/>
-      </Modal>
+
+      <div>
+        <button className='gameChatBTN'onClick={setModalTrue}>Game Chat</button>
+        <Modal isOpen={modalIsOpen} shouldCloseOnOverlayClick={true} onRequestClose={()=> setModalIsOpen(false)} style={customStyles}>
+            <button onClick={setModalFalse}>x</button>
+            <GameChat/>
+        </Modal>
+      </div>
 
     </div>
   )
